a58283d4acded80f91567193250aaff64f316e0d
ryohei udagawa <r.udagawa1995@gmail.com>
2020-04-14 19:54:57 +0900

    test

---
+++ test/test.rb
@@ -0,0 +1,11 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.start
