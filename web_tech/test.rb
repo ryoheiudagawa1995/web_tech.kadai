5a62b75c8b12d0c87c375743960c4e5341f43327
ryohei udagawa <r.udagawa1995@gmail.com>
2020-04-15 15:29:26 +0900

    修正

--- web_tech/test.rb
+++ web_tech/test.rb
@@ -10,7 +10,7 @@ server = WEBrick::HTTPServer.new({
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
-server.mount('/lunch', WEBrick::HTTPServlet::ERBHandler, 'lunch.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'lunch.html.erb')
 server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya1.rb')
 server.mount('/goya2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya2.rb')
 server.start
